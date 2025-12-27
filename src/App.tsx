6ed63c3f83ea434d4904e05b35f27f80590102a7
h.nevisandegan <h.nevisandegan@ip-jam.com>
2025-12-27 19:07:45 +0330

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container" dir="rtl">
+      <div className="right-top">card</div>
+      <div className="right-bottom">like</div>
+      <div className="left">form</div>
+    </div>
+  );
+}
+
+export default App;
