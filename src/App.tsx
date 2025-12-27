f640504aa5c425efb377e57942129c8f21b50609
h.nevisandegan <h.nevisandegan@ip-jam.com>
2025-12-27 19:20:50 +0330

    Implementation card like form components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import "./App.css";
+import Card from "./components/card";
+import Form from "./components/form";
+import Like from "./components/like";
 
 function App() {
   return (
     <div className="container" dir="rtl">
-      <div className="right-top">card</div>
-      <div className="right-bottom">like</div>
-      <div className="left">form</div>
+      <div className="right-top">
+        <Card />
+      </div>
+      <div className="right-bottom">
+        <Like />
+      </div>
+      <div className="left">
+        <Form />
+      </div>
     </div>
   );
 }
